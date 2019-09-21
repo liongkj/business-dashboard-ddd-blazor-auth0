780eae786a91a47b5d19baab1073e90e1f6e9788
KJ <fantasticbabii@gmail.com>
2019-09-21 20:47:33 +0800

    first commit

---
+++ JomMalaysia.Framework/Constant/APIConstant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JomMalaysia.Framework.Constant
+{
+    public partial class APIConstant
+    {
+        public class API
+        {
+            public class Path
+            {
+                public const string Category = "api/Categories";
+
+                public const string Subcategory = "api/Categories/{catName}";
+
+                public const string Listing = "api/Listings";
+
+                public const string Image = "api/Images";
+
+            }
+        }
+    }
+}
---
+++ JomMalaysia.Presentation/Gateways/Category/ICategoryGateway.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JomMalaysia.Framework.WebServices;
+using JomMalaysia.Presentation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JomMalaysia.Presentation.Gateways.Category
+{
+    public interface ICategoryGateway
+    {
+        Task<List<CategoryViewModel>> GetCategories();
+        Task<IWebServiceResponse> CreateCategory(CategoryViewModel vm);
+        Task<IWebServiceResponse> Delete(string CategoryName);
+    }
+}
