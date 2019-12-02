62a41f0a85c3317bb78520ff292d06ad718b9c90
khaijiet <fantasticbabii@gmail.com>
2019-12-02 13:32:00 +0800

    refactor listing image

--- JomMalaysia.Presentation/ViewModels/Listings/ListingImageViewModel.cs
+++ JomMalaysia.Presentation/ViewModels/Listings/ListingImageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 using JomMalaysia.Presentation.Models.Common;
@@ -8,7 +9,9 @@ namespace JomMalaysia.Presentation.ViewModels.Listings
 {
     public class ListingImageViewModel
     {
+        [DisplayName("Logo")]
         public Image ListingLogo { get; set; }
+        [DisplayName("Cover Photo")]
         public Image CoverPhoto { get; set; }
         public List<Image> ListingDetails { get; set; }
 
