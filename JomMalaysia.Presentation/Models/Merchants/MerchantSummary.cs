e0c3fa9e4526be71c97dcbfac23fc9250347cd28
liongkj <fantasticbabii@gmail.com>
2020-03-05 00:00:18 +0800

    update merchant

--- JomMalaysia.Presentation/Models/Merchants/MerchantSummary.cs
+++ JomMalaysia.Presentation/Models/Merchants/MerchantSummary.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel;
+
 namespace JomMalaysia.Presentation.Models.Merchants
 {
     public class MerchantSummary
     {
+        [DisplayName("SSM (New)")]
         public string SsmId { get; set; }
+        [DisplayName("Old")]
         public string OldSsmId { get; set; }
+        [DisplayName("Name")]
         public string RegistrationName { get; set; }
         public string MerchantId { get; set; }
 
